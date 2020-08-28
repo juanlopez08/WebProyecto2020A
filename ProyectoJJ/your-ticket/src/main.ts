4cf23f97f8961484e7961ae93a3b96a8ed8b9910
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-08-28 16:50:07 -0500

    Entorno revisado

--- ProyectoJJ/your-ticket/src/main.ts
+++ ProyectoJJ/your-ticket/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const express = require('express')
+//const cookieParser = require('cookie-parser')
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule) as any;
+
+  /*
+  AQUI CONFIGURACION
+  ANTES DEL APP.LISTEN()
+  */
+
+  //para inseguras y seguras
+  //app.use(cookieParser())
+
+  //para cookies firmadas
+  //app.use(cookieParser('Me gustan los poliperros'));
+
+  //app.set('view engine', 'ejs')
+ // app.use(express.static('publico'));
   await app.listen(3000);
 }
 bootstrap();
